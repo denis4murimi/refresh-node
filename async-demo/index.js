9a23167080ba364955eb487bec3c2ed0b10e2cf1
Denis Murimi <denis4murimi@gmail.com>
2023-11-09 16:26:55 +0300

    add: implemented async/await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,9 +1,34 @@
 console.log('start');
 
-getUser(1)
+// Callback approach
+/* getUser(1, (user) => {
+    console.log(user);
+    getRepos(user.githubUsername, (repos) => {
+        console.log(repos);
+    });
+}); */
+
+// Promises approach
+/* getUser(1)
     .then(user => getRepos(user.githubUsername))
     .then(repos => console.log(repos[0]))
     .catch(err => console.log(new Error('message')));
+ */
+
+// Async / Await Approach
+async function displayFirstRepo() {
+    try {
+        const user = await getUser(1);
+        console.log(user.githubUsername);
+        
+        const repos = await getRepos(user.githubUsername);
+        console.log(repos[0]);
+    } catch (error) {
+        console.log('Error: ', error.message);
+    }
+}
+
+displayFirstRepo();
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
@@ -18,8 +43,9 @@ function getUser(id) {
 function getRepos(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('getting repos...');
+            console.log('Calling Github api...');
             resolve(['repo 1', 'repo 2', 'repo 3']);
+            //reject(new Error('Could not get repos'));
         })
     });
 }
